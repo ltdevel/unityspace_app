f12a5dbbaa7266e6045e39292907717f64ae8dda
devmaslove <63534992+devmaslove@users.noreply.github.com>
2023-09-08 18:04:30 +0300

    Установил шрифт Roboto

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'UnitySpace',
       theme: ThemeData(
+        fontFamily: 'Roboto',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
