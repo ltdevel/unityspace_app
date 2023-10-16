94f06bdeee69e7fde90954c18cfe5c7d3f4ec0e5
devmaslove <63534992+devmaslove@users.noreply.github.com>
2023-10-16 17:24:57 +0300

    Обновил файлы сборщика

--- lib/screens/login_by_email_screen.dart
+++ lib/screens/login_by_email_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:unityspace/widgets/main_form_button_widget.dart';
 import 'package:unityspace/widgets/main_form_logo_widget.dart';
 import 'package:unityspace/widgets/main_form_text_title_widget.dart';
