30bf3da186747537349f0cdde453d4484b741713
devmaslove <63534992+devmaslove@users.noreply.github.com>
2023-11-17 20:08:52 +0300

    feat: Вынес кнопку выйти из аккаунта в шапку

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:unityspace/screens/app_navigaion_drawer.dart';
-import 'package:unityspace/screens/widgets/color_button_widget.dart';
 import 'package:unityspace/store/auth_store.dart';
 import 'package:wstore/wstore.dart';
 
 class AccountScreenStore extends WStore {
-  // TODO: add data here...
+  bool isExiting = false;
 
   @override
   AccountScreen get widget => super.widget as AccountScreen;
@@ -24,20 +24,30 @@ class AccountScreen extends WStoreWidget<AccountScreenStore> {
     return Scaffold(
       drawer: const AppNavigationDrawer(),
       appBar: AppBar(
-        title: const Text('Аккаунт'),
+        title: const Text('Мой профиль'),
+        actions: [
+          IconButton(
+            padding: const EdgeInsets.all(2),
+            icon: SvgPicture.asset(
+              'assets/icons/sign_out.svg',
+              width: 20,
+              height: 20,
+              theme: SvgTheme(
+                currentColor:
+                    IconTheme.of(context).color ?? const Color(0xFF111012),
+              ),
+            ),
+            tooltip: 'Выйти из аккаунта',
+            onPressed: () async {
+              if (store.isExiting) return;
+              store.isExiting = true;
+              await AuthStore().signOut();
+              store.isExiting = false;
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: ColorButtonWidget(
-          width: double.infinity,
-          onPressed: () {
-            AuthStore().signOut();
-          },
-          text: 'Выйти из аккаунта',
-          loading: false,
-          colorBackground: const Color(0xFF111012),
-          colorText: Colors.white.withOpacity(0.9),
-        ),
-      ),
+      body: Container(),
     );
   }
 }
