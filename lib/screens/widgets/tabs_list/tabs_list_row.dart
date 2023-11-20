1c0c98b0fdfa156dbe1908a958f19ccf63f49bd9
devmaslove <63534992+devmaslove@users.noreply.github.com>
2023-11-20 22:29:15 +0300

    feat: Сделал чтобы проскролливалось до кнопки при нажатии

--- lib/screens/widgets/tabs_list/tabs_list_row.dart
+++ lib/screens/widgets/tabs_list/tabs_list_row.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:unityspace/screens/widgets/tabs_list/tab_button.dart';
 
-class TabsListRow extends StatelessWidget {
+class TabsListRow extends StatefulWidget {
   final List<TabButton> children;
 
   const TabsListRow({
@@ -10,13 +10,59 @@ class TabsListRow extends StatelessWidget {
   });
 
   @override
+  State<TabsListRow> createState() => _TabsListRowState();
+}
+
+class _TabsListRowState extends State<TabsListRow> {
+  late List<GlobalKey> tabsKeys;
+
+  @override
+  void initState() {
+    super.initState();
+    tabsKeys = List.generate(widget.children.length, (index) => GlobalKey());
+  }
+
+  @override
+  void didUpdateWidget(covariant TabsListRow oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.children.length > tabsKeys.length) {
+      final int delta = widget.children.length - tabsKeys.length;
+      tabsKeys.addAll(List<GlobalKey>.generate(delta, (_) => GlobalKey()));
+    } else if (widget.children.length < tabsKeys.length) {
+      tabsKeys.removeRange(widget.children.length, tabsKeys.length);
+    }
+    final selectedTab = widget.children.indexWhere((tab) => tab.selected);
+    final selectedTabOld = oldWidget.children.indexWhere((tab) => tab.selected);
+    if (selectedTab != selectedTabOld) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        final selectedTabContext = tabsKeys[selectedTab].currentContext;
+        if (selectedTabContext != null) {
+          Scrollable.ensureVisible(
+            selectedTabContext,
+            duration: const Duration(milliseconds: 300),
+            alignment: 0.5,
+          );
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
           const SizedBox(width: 16),
-          ...children.expand((tab) => [tab, const SizedBox(width: 8)]),
+          ...widget.children.expand(
+            (tab) => [
+              SizedBox(
+                key: tabsKeys[widget.children.indexOf(tab)],
+                child: tab,
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
           const SizedBox(width: 8),
         ],
       ),
