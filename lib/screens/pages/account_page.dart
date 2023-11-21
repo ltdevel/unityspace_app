01f488c88dae1a9f6baebdcd55fd6a43c82dfa4a
devmaslove <63534992+devmaslove@users.noreply.github.com>
2023-11-21 20:43:38 +0300

    feat: Адаптивная верстка страницы Аккаунт

--- lib/screens/pages/account_page.dart
+++ lib/screens/pages/account_page.dart
@@ -18,11 +18,101 @@ class AccountPage extends WStoreWidget<AccountPageStore> {
 
   @override
   Widget build(BuildContext context, AccountPageStore store) {
-    return Container(
-      color: Colors.blue,
-      child: const Center(
-        child: Text('Аккаунт'),
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+          ),
+          constraints: const BoxConstraints(maxWidth: 660),
+          child: AccountContentWidget(
+            avatar: Container(
+              width: 40,
+              height: 40,
+              color: Colors.deepPurple,
+            ),
+            children: const [
+              Text('Имя'),
+              Text('День рождения'),
+              Text('Email'),
+              Text('Телефон'),
+              Text('Ссылка на профиль в Telegram'),
+              Text('Ссылка на профиль в Github'),
+              Text('Пароль'),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class AccountContentWidget extends StatelessWidget {
+  final Widget avatar;
+  final List<Widget> children;
+
+  const AccountContentWidget({
+    super.key,
+    required this.avatar,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 420) {
+          return Column(
+            children: [
+              avatar,
+              const SizedBox(height: 16),
+              const Divider(
+                height: 1,
+                thickness: 1,
+                color: Color(0xFFE5E7EB),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.infinity,
+                child: Wrap(
+                  spacing: 16,
+                  direction: Axis.vertical,
+                  clipBehavior: Clip.hardEdge,
+                  children: children,
+                ),
+              ),
+            ],
+          );
+        }
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            avatar,
+            const SizedBox(width: 32),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.only(left: 16),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                      color: Color(0xFFE5E7EB),
+                    ),
+                  ),
+                ),
+                child: Wrap(
+                  spacing: 16,
+                  direction: Axis.vertical,
+                  clipBehavior: Clip.hardEdge,
+                  children: children,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
