8377145622682b41d5f981df1c2e2e4ac1d3ddaf
Marina Bukhanovskaya <116250199+bukhanovskayamarina22@users.noreply.github.com>
2024-05-07 12:59:25 +0300

    refactor: добавлены enum TaskChangesTypes и модель TaskHistory (#7)

--- lib/models/task_models.dart
+++ lib/models/task_models.dart
@@ -1,5 +1,75 @@
 import 'dart:core';
 
+enum TaskChangesTypes {
+  createTask(0),
+  changeName(1),
+  changeDescription(2),
+  changeResponsible(3),
+  changeStage(4),
+  changeColor(5),
+  changeStatus(6),
+  changeDate(7),
+  sendMessage(8),
+  addTag(9),
+  deleteTag(10),
+  deleteTask(11),
+  changeBlockReason(12),
+  addCover(13),
+  deleteCover(14),
+  overdueTaskNoResponsible(15),
+  overdueTaskWithResponsible(16),
+  changeImportance(17),
+  commit(18),
+  addStage(19),
+  deleteStage(20),
+  removeMember(21),
+  addResponsible(22),
+  removeResponsible(23);
+
+  final int value;
+
+  const TaskChangesTypes(this.value);
+}
+
+class TaskHistory {
+  final int id;
+  final String? state;
+  final int taskId;
+  final TaskChangesTypes type;
+  final String updateDate;
+  final int userId;
+  final String? projectName;
+  final String? taskName;
+  final String? commitName;
+
+  TaskHistory({
+    required this.id,
+    required this.state,
+    required this.taskId,
+    required this.type,
+    required this.updateDate,
+    required this.userId,
+    required this.projectName,
+    required this.taskName,
+    required this.commitName,
+  });
+
+  factory TaskHistory.fromResponse(TaskHistoryResponse response) {
+    return TaskHistory(
+      id: response.id,
+      state: response.state,
+      taskId: response.taskId,
+      type: TaskChangesTypes.values
+          .firstWhere((type) => type.value == response.type),
+      updateDate: response.updateDate,
+      userId: response.userId,
+      projectName: response.projectName,
+      taskName: response.taskName,
+      commitName: response.commitName,
+    );
+  }
+}
+
 class MyTaskHistoryResponse {
   final int maxPageCount;
   final List<TaskHistoryResponse> history;
